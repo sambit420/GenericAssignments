1129438155df135fef9246d2ec05b17d4ce09399
sambit420 <sambit2024@gmail.com>
2022-10-30 20:59:11 +0530

    Added : Display Welcome to Generic Assignment Problem

---
+++ src/com/generics/assignments/TestMaximum.java
@@ -0,0 +1,15 @@
+package com.generics.assignments;
+
+public class TestMaximum {
+    //Default Constructor
+    public TestMaximum()
+    {
+        System.out.println("Welcome to Generic Assignment Problem.");
+    }
+
+    public static void main(String[] args) {
+        //Initialization of object.
+        TestMaximum testMaximum = new TestMaximum();
+    }
+}
+
