abd5318a9bddb46eabdc2651a6a947ebdac4b557
sambit420 <sambit2024@gmail.com>
2022-10-31 23:40:02 +0530

    Added : Use Case 5 Program - Extend the max method to also print the

--- src/com/generics/assignments/TestMaximum.java
+++ src/com/generics/assignments/TestMaximum.java
@@ -5,19 +5,23 @@
 //Refactoring 1 - all 3 to one generic method and find out the maximum.
 //Refactoring 2 - to take 3 variables of generic types and find out maximum.
 //Use Case 4 is to extend the max method to take more than three parameters.
+//Use Case 5 is to extend the max method to also print the max to std out using Generic Method.
+
 package com.generics.assignments;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class TestMaximum <T> {
+public class TestMaximum <T extends Comparable<T>> {
+
     //Creating 3 variables.
-    T[] typeArray;
+    T x, y, z;
 
     //Creating Parameterized constructor for generic data types.
-    public TestMaximum(T[] typeArray)
-    {
-        this.typeArray = typeArray;
+    public TestMaximum(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     //Default Constructor
@@ -26,49 +30,61 @@ public class TestMaximum <T> {
         System.out.println("Welcome to Generic Assignment Problem.");
     }
 
+    public T maximum()
+    {
+        return TestMaximum.maximum(x, y, z);
+    }
+
+    // determines the largest of three Comparable objects
+    public static <T extends Comparable<T>> T maximum(T x, T y, T z)
+    {
+        T max = x; // assume x is initially the largest
+
+        if (y.compareTo(max) > 0)
+        {
+            max = y; // y is the largest so far
+        }
+        if (z.compareTo(max) > 0)
+        {
+            max = z; // is the largest now
+        }
+        printMax(x, y, z, max);
+        return max; // returns the largest object
+    }
+
+    public static String testMaximum(String x, String y, String z)
+    {
+        String max = x;
+        if(y.compareTo(max) > 0)
+        {
+            max = y; // y is the largest so far
+        }
+        if(z.compareTo(max) > 0)
+        {
+            max = z;// is the largest
+        }
+        //now printMax(x, y, z, max);
+        printMax(x, y, z, max);
+        return max; // returns the largest object
+    }
+
+    public static <T> void printMax(T x, T y, T z, T max)
+    {
+        System.out.printf("Max of %s, %s and %s is %s\n", x, y, z, max);
+    }
+
     public static void main(String[] args) {
         //Initialization of object.
         TestMaximum testMaximum = new TestMaximum();
 
         //Define the array where size is 3(given).
-        Integer[] intArray = {5,6,8,7,9};
-        Float[] floatArray = {8.5f,5.5f,2.3f,3.9f,6.4f,1.2f};
-        String[] strArray = {"Apple","Peach","Banana","Guava","Papaya"};
+        Integer xInt = 5, yInt = 6, zInt = 7;
+        Float xFlt = 5.5f, yFlt = 4.5f, zFlt = 3.5f;
+        String xStr = "Apple", yStr = "Peach", zStr = "Banana";
 
-        getMaximum(new TestMaximum(intArray));
-        getMaximum(new TestMaximum(floatArray));
-        getMaximum(new TestMaximum(strArray));
-
-    }
-
-    //Get maximum from the 3 variables.
-    public static <T extends Comparable<T>> void getMaximum(TestMaximum testMaximum) {
-        Arrays.sort(testMaximum.typeArray);
-        T max = (T) testMaximum.typeArray[0];
-        int position = 0;
-        for (int i = 0; i < testMaximum.typeArray.length; i++)
-
-        {
-            T a = (T)testMaximum.typeArray[i];
-            int b = a.compareTo(max);
-            if(b > 0)
-            {
-                max = (T) testMaximum.typeArray[i];
-                position = i;
-            }
-        }
-        System.out.println("Array is sorted.");
-        System.out.println("\nMaximum integer is : "+max);
-        System.out.println("Maximum integer position is : "+position+" in array.");
-        printArray(testMaximum.typeArray);
-
-    }
-
-    //Print the maximum
-    public static void printArray(Object[] typeArray) {
-        for (Object element : typeArray)
-        {
-            System.out.println(element+"  ");
+        TestMaximum.testMaximum(xStr, yStr, zStr);
+        new TestMaximum(xInt, yInt, zInt).maximum();
+        new TestMaximum(xFlt, yFlt, zFlt).maximum();
+        new TestMaximum(xStr, yStr, zStr).maximum();
         }
     }
-}
